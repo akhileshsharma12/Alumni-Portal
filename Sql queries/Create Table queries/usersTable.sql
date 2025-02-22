5d5646fb57b97aed839df65a390edc023f1ac167
techbooster23 <akashp8405@gmail.com>
2025-02-22 15:32:00 +0530

    db initialization

---
+++ Sql queries/Create Table queries/usersTable.sql
@@ -0,0 +1,9 @@
+CREATE TABLE users (
+    id UUID DEFAULT gen_random_uuid() PRIMARY KEY,
+    name VARCHAR(100) NOT NULL,
+    email VARCHAR(255) UNIQUE NOT NULL,
+    password TEXT NOT NULL,
+    role VARCHAR(50) CHECK (role IN ('alumni', 'admin','student')) NOT NULL,
+    is_verified BOOLEAN DEFAULT FALSE,
+    created_at TIMESTAMP DEFAULT NOW()
+);
--- backend/app.js
+++ backend/app.js
@@ -1,15 +1,23 @@
 import bodyParser from 'body-parser';
 import express from 'express';
+import db from './config/db.config.js';
 
 const app = express();
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({extended:true}));
+db.connect();
 
-app.get("/",(req,res)=>{
-    res.send("<h1>Server started successFully</h1>");
+app.get("/", async (req,res)=>{
+    try {
+       
+        const result = await db.query("SELECT * FROM users");
+        res.json(result.rows);
+    } catch (error) {
+        console.log(error);
+    }
 });
 
 app.listen(3000,()=>{
     console.log("server is running");
-})
+});
